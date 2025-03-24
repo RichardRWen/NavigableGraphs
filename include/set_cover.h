f22cc99be3555e65d4e7608cd3b3cb4e62f23302
Richard Wen <rwen1@umd.edu>
2025-03-24 13:45:19 -0400

    Implemented sampling-based set cover

--- include/set_cover.h
+++ include/set_cover.h
@@ -1,12 +1,17 @@
 #pragma once
 
 #include <iostream>
+#include <cstdint>
 #include <utility>
+#include <vector>
+#include <set>
+#include <limits>
 #include <mutex>
 
 #include <parlay/sequence.h>
 #include <parlay/parallel.h>
 #include <parlay/primitives.h>
+#include <parlay/random.h>
 
 #include <utils/point_range.h>
 
@@ -30,6 +35,16 @@ public:
                 ranks[dists[j]] = j;
             }
         }, 1);
+    }
+
+    uint32_t rank_of(uint32_t i, uint32_t j) {
+        // Return the rank of point j in the sorted list of distances from point i
+        return sorted_ranks[i * points.size() + j];
+    }
+
+    bool closer_than(uint32_t i, uint32_t j, uint32_t k) {
+        // Return true if i is closer to j than to k
+        return rank_of(i, j) < rank_of(i, k);
     }
 
     // std::mutex cout_lock;
@@ -100,6 +115,67 @@ public:
         }, 1);*/
         auto adjlists = parlay::tabulate(points.size(), [&](size_t i) {
             return adjlist_greedy(i);
+        });
+        return adjlists;
+    }
+
+    std::vector<uint32_t> adjlist_sampling(uint32_t v, parlay::random_generator &gen) {
+        std::vector<uint32_t> adjlist;
+        std::vector<uint32_t> uncovered_points;
+        std::unordered_map<uint32_t, uint32_t> uncovered_indices;
+        uncovered_points.reserve(points.size() - 1);
+        uncovered_indices.reserve(points.size() - 1);
+        for (uint32_t i = 0; i < points.size(); i++) {
+            if (i != v) {
+                uncovered_points.push_back(i);
+                uncovered_indices[i] = uncovered_points.size() - 1;
+            }
+        }
+
+        // Compute an approximate set cover with logn expected size
+        // While there are uncovered points, sample a constant number of uncovered points
+        // Pick the set which covers the most sampled points
+        // Mark all of the points covered by that set as covered
+        // Repeat until all points are covered
+        std::uniform_int_distribution<uint32_t> dist(0, std::numeric_limits<uint32_t>::max());
+        while (!uncovered_points.empty()) {
+            std::vector<uint32_t> votes(points.size(), 0);
+            for (size_t i = 0; i < 10; i++) {
+                uint32_t sample_index = dist(gen) % uncovered_points.size();
+                uint32_t sample_point = uncovered_points[sample_index];
+                uint32_t *indices = sorted_dists.indices.begin() + sample_point * points.size();
+                uint32_t set_boundary = rank_of(sample_point, v);
+                for (uint32_t j = 0; j < set_boundary; j++) {
+                    uint32_t set_index = indices[j];
+                    votes[set_index]++;
+                }
+            }
+
+            auto best_set = std::max_element(votes.begin(), votes.end());
+            if (*best_set == 0) {
+                std::cerr << "Error: Unable to cover all points." << std::endl;
+                abort();
+            }
+            uint32_t set_index = best_set - votes.begin();
+            adjlist.push_back(set_index);
+
+            for (uint32_t j : uncovered_points) {
+                if (closer_than(j, set_index, v)) {
+                    std::swap(uncovered_points[uncovered_indices[j]], uncovered_points.back());
+                    uncovered_indices[uncovered_points.back()] = uncovered_indices[j];
+                    uncovered_points.pop_back();
+                }
+            }
+        }
+
+        return adjlist;
+    }
+
+    parlay::sequence<std::vector<uint32_t>> adjlists_sampling() {
+        parlay::random_generator gen();
+        auto adjlists = parlay::tabulate(points.size(), [&](size_t i) {
+            auto r = gen[i];
+            return adjlist_sampling(i, r);
         });
         return adjlists;
     }
